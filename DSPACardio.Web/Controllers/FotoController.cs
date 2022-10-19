01e21db71eb9a7cc3ee73c46fd308c3c02469b0c
Miguel Herrera <sanchezherreraangelmiguel@gmail.com>
2022-10-19 15:39:16 -0500

    Hicimos los controladores y vistas

---
+++ DSPACardio.Web/Controllers/FotoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSPACardio.Web.Controllers
+{
+    public class FotoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
