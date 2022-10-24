ee4554bd8372fbaf730003e58f4defd2a538be07
Miguel Herrera <sanchezherreraangelmiguel@gmail.com>
2022-10-24 11:45:44 -0500

    Creamos clases de Data, Entities y DataContext, se instalaron un paquete Nuget

---
+++ DSPACardio.Web/Data/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DSPACardio.Web.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+    }
+}
---
+++ DSPACardio.Web/Data/Entities/Patient.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSPACardio.Web.Data.Entities
+{
+    public class Patient : User
+    {
+        public int Id { get; set; }
+
+        //public User User { get; set; }
+    }
+}
